b09bc50f661808875cd384c1b76a5eb278240754
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-11-03 00:26:33 +0000

    mobile robot tutorial done

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 from setuptools import find_packages, setup
 
 package_name = 'pick_and_toss'
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name + '/launch', ['launch/robot_launch.py']))
+data_files.append(('share/' + package_name + '/worlds', ['worlds/my_world.wbt']))
+data_files.append(('share/' + package_name + '/resource', ['resource/my_robot.urdf']))
+data_files.append(('share/' + package_name, ['package.xml']))
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    packages=[package_name],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='yg5d6@mail.missouri.edu',
+    maintainer='user',
+    maintainer_email='user.name@mail.com',
     description='TODO: Package description',
-    license='Apache-2.0',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
@@ -24,3 +26,4 @@ setup(
         ],
     },
 )
+
