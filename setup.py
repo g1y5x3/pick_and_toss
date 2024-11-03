ea0557a6da4ca485003c682811b9b00099e2bfbc
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-11-03 03:15:44 +0000

    initial environment for ure3 robot

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import find_packages, setup
 package_name = 'pick_and_toss'
 data_files = []
 data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
-data_files.append(('share/' + package_name + '/launch', ['launch/robot_launch.py']))
-data_files.append(('share/' + package_name + '/worlds', ['worlds/my_world.wbt']))
+data_files.append(('share/' + package_name + '/launch',   ['launch/robot_launch.py', 
+                                                           'launch/ure3_launch.py']))
+data_files.append(('share/' + package_name + '/worlds',   ['worlds/my_world.wbt',
+                                                           'worlds/ure3_world.wbt']))
 data_files.append(('share/' + package_name + '/resource', ['resource/my_robot.urdf']))
 data_files.append(('share/' + package_name, ['package.xml']))
 
