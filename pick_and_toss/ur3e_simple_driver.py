68eab9eadb1b5b310e6db3752e5941f1ed287e43
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-11-06 19:14:32 +0000

    complete the simple driver with 3 joints

--- pick_and_toss/ur3e_simple_driver.py
+++ pick_and_toss/ur3e_simple_driver.py
@@ -4,19 +4,31 @@ from std_msgs.msg import Float64
 class UR3eArmDriver:
     def init(self, webots_node, properties):
         self.__robot = webots_node.robot
-        self.__shoulder_lift_joint = self.__robot.getDevice('shoulder_lift_joint')
-        self.__shoulder_lift_joint.setPosition(-1.57)
 
-        self.__target_shoulder_lift_joint = Float64()
+        self.__shoulder_lift_joint = self.__robot.getDevice("shoulder_lift_joint")
+        self.__shoulder_pan_joint = self.__robot.getDevice("shoulder_pan_joint")
+        self.__elbow_joint = self.__robot.getDevice("elbow_joint")
+
+        self.__shoulder_lift_joint.setPosition(-1.57)
+        self.__shoulder_pan_joint.setPosition(0.78)
+        self.__elbow_joint.setPosition(1.57)
 
         rclpy.init(args=None)
-        self.__node = rclpy.create_node('ur3e_driver')
-        self.__node.create_subscription(Float64, 'ur3e/joints/shoulder_lift_joint', self.__shoulder_lift_joint_callback, 1)
 
-    def __shoulder_lift_joint_callback(self, msg):
-        self.__target_shoulder_lift_joint = msg
-        shoulder_lift_joint = self.__target_shoulder_lift_joint.data
-        self.__shoulder_lift_joint.setPosition(shoulder_lift_joint)
+        self.__node = rclpy.create_node("ur3e_driver")
+
+        self.__node.create_subscription(Float64, 'ur3e/joints/shoulder_lift_joint', self.__shoulder_lift_joint_callback, 1)
+        self.__node.create_subscription(Float64, 'ur3e/joints/shoulder_pan_joint', self.__shoulder_pan_joint_callback, 1)
+        self.__node.create_subscription(Float64, 'ur3e/joints/elbow_joint', self.__elbow_joint_callback, 1)
+
+    def __shoulder_lift_joint_callback(self, msg: Float64):
+        self.__shoulder_lift_joint.setPosition(msg.data)
+    
+    def __shoulder_pan_joint_callback(self, msg):
+        self.__shoulder_pan_joint.setPosition(msg.data)
+
+    def __elbow_joint_callback(self, msg):
+        self.__elbow_joint.setPosition(msg.data)
 
     def step(self):
         rclpy.spin_once(self.__node, timeout_sec=0)
