846c67947d74e8ea972aa6f57d020496e4b8411b
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-11-06 14:34:24 +0000

    modify driver to stop setPosition constantly

--- pick_and_toss/ur3e_simple_driver.py
+++ pick_and_toss/ur3e_simple_driver.py
@@ -5,6 +5,7 @@ class UR3eArmDriver:
     def init(self, webots_node, properties):
         self.__robot = webots_node.robot
         self.__shoulder_lift_joint = self.__robot.getDevice('shoulder_lift_joint')
+        self.__shoulder_lift_joint.setPosition(-1.57)
 
         self.__target_shoulder_lift_joint = Float64()
 
@@ -14,8 +15,8 @@ class UR3eArmDriver:
 
     def __shoulder_lift_joint_callback(self, msg):
         self.__target_shoulder_lift_joint = msg
+        shoulder_lift_joint = self.__target_shoulder_lift_joint.data
+        self.__shoulder_lift_joint.setPosition(shoulder_lift_joint)
 
     def step(self):
         rclpy.spin_once(self.__node, timeout_sec=0)
-        shoulder_lift_joint = self.__target_shoulder_lift_joint.data
-        self.__shoulder_lift_joint.setPosition(shoulder_lift_joint)
