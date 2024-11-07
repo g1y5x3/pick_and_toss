2ed8b0470191d3851a4d70150b196273de3182f1
Yixiang Gao <ygao@missouri.edu>
2024-11-07 16:47:13 -0600

    adding joint state publisher for the arms

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@ from setuptools import setup
 package_name = 'pick_and_toss'
 data_files = []
 data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
-data_files.append(('share/' + package_name + '/launch',   ['launch/robot_launch.py', 
-                                                           'launch/ur3e_simple_driver_launch.py']))
-data_files.append(('share/' + package_name + '/worlds',   ['worlds/my_world.wbt',
-                                                           'worlds/ur3e_world.wbt']))
-data_files.append(('share/' + package_name + '/resource', ['resource/my_robot.urdf',
-                                                           'resource/ur3e_arms_simple.urdf', 'resource/ur3e_cameras.urdf']))
+data_files.append(('share/' + package_name + '/launch',   ['launch/ur3e_simple_driver_launch.py']))
+data_files.append(('share/' + package_name + '/worlds',   ['worlds/ur3e_world.wbt']))
+data_files.append(('share/' + package_name + '/resource', ['resource/ur3e_arms_simple.urdf', 'resource/ur3e_cameras.urdf']))
 data_files.append(('share/' + package_name, ['package.xml']))
 
 setup(
